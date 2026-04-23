873d9151cdb53a64ff276f1ef348f6afd1b413f2
Kenluckyz <aoken0991242405@gmail.com>
2026-04-23 11:32:41 +0700

    finish

--- Assets/Script/Player2DController.cs
+++ Assets/Script/Player2DController.cs
@@ -4,10 +4,10 @@ using UnityEngine.InputSystem;
 public class Player2DController : MonoBehaviour
 {
     public float speed = 5.0f;
-    public float jumpForce = 450; // Jump
+    public float jumpForce = 450; 
 
     private Rigidbody2D _rb;
-    private float _moveInputValue; // A=(-1), D=1
+    private float _moveInputValue; 
     private bool _isGrounded;
     private SpriteRenderer _spriteRenderer;
     void Start()
@@ -19,18 +19,17 @@ public class Player2DController : MonoBehaviour
     {
         if (Keyboard.current != null)
         {
-            _moveInputValue = (Keyboard.current.dKey.isPressed ? 1 : 0) // กด A, D เพื่อกำหนดทิศทาง
+            _moveInputValue = (Keyboard.current.dKey.isPressed ? 1 : 0) 
                 - (Keyboard.current.aKey.isPressed ? 1 : 0);
         }
-        _rb.linearVelocity = new Vector2(_moveInputValue * speed, _rb.linearVelocity.y); //ใส่แรง X คงแรง Y
+        _rb.linearVelocity = new Vector2(_moveInputValue * speed, _rb.linearVelocity.y); 
 
         if (_moveInputValue < 0) { _spriteRenderer.flipX = true; }
         else if (_moveInputValue > 0) { _spriteRenderer.flipX = false; }
 
-        if (Keyboard.current.spaceKey.wasPressedThisFrame && _isGrounded) // กด spacebar พร้อมเช็คติดพื้น 
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && _isGrounded) 
         {
-            _rb.AddForce(new Vector2(_rb.linearVelocity.x, jumpForce)); // ใส่แรงแกน Y คงแรงแกน X
-        }
+            _rb.AddForce(new Vector2(_rb.linearVelocity.x, jumpForce)); 
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
